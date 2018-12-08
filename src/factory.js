2f55eb3c4b42767301331be5d28ef84e9a7b367c
Alex898 <myemail@mail.ru>
2018-12-08 14:14:08 +0300

    first commit

---
+++ src/factory.js
@@ -0,0 +1,65 @@
+import News from "./news.js";
+
+export default class FactoryReguest {
+	
+	constructor(typeOfRequest, url) {
+		//proxy
+		let paramReguest = {};
+
+		let proxy = new Proxy(paramReguest, {
+			get(target, prop) {
+			console.log(`Чтение ${prop}`);
+			return target[prop];
+		},
+		set(target, prop, value) {
+			console.log(`Запись ${prop} ${value}`);
+			target[prop] = value;
+			return true;
+		}
+		});
+		proxy.typeOfRequest = typeOfRequest;
+		proxy.url = url;
+		
+		//factory
+		this.req;
+        if (typeOfRequest === "request") {
+            this.req = new Request(url);
+			this.typeOfRequest = "request";
+			this.newRequest();
+        }
+		else if (typeOfRequest === "get"){
+			this.req = new XMLHttpRequest();
+			this.typeOfRequest = "get";
+			this.newGet();
+		}
+		else if (typeOfRequest === "post"){
+			this.req = new XMLHttpRequest();
+			this.typeOfRequest = "post";
+			this.newPost();
+		}
+	}
+	
+	
+			
+	
+	newRequest(){
+		
+		let  source = new News(this.typeOfRequest, this.req);
+		source.printSource();
+
+		let news = new News(this.req);
+		news.printNews();
+	}
+	
+	newGet(){}
+
+	newPost(){}
+};
+
+
+
+
+
+
+	
+
